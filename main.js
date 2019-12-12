e4a65b3055a0e2716d2d79ebc70fb3f34c7f3a0a
wasim varshani <wasim.varshani@gmail.com>
2019-12-12 12:13:53 +0530

    remove commented code

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
 (function () {
 
-
     const localTime = document.getElementById('local-time');
-    const clHr = localTime.getElementsByTagName('circle')[0];
+    const clSec = localTime.getElementsByTagName('circle')[0];
     const clMin = localTime.getElementsByTagName('circle')[1];
-    const clSec = localTime.getElementsByTagName('circle')[2];
+    const clHr = localTime.getElementsByTagName('circle')[2];
 
-    const handHr = localTime.getElementsByTagName('line')[0];
+    const handSec = localTime.getElementsByTagName('line')[0];
     const handMin = localTime.getElementsByTagName('line')[1];
-    const handSec = localTime.getElementsByTagName('line')[2];
+    const handHr = localTime.getElementsByTagName('line')[2];
 
     const DATE = localTime.getElementsByTagName('text')[0];
     const DAY = localTime.getElementsByTagName('text')[1];
 
-    const utcTime = document.getElementById('utc-time');
-    const uHr = utcTime.getElementsByTagName('circle')[0];
-    const uMin = utcTime.getElementsByTagName('circle')[1];
-    const uSec = utcTime.getElementsByTagName('circle')[2];
-
-    const utcHandHr = utcTime.getElementsByTagName('line')[0];
-    const utcHandMin = utcTime.getElementsByTagName('line')[1];
-    const utcHandSec = utcTime.getElementsByTagName('line')[2];
-
     setInterval(drawClock, 1000);
     fillArc(clSec, clMin, clHr);
-    fillArc(uSec, uMin, uHr);
 
     function drawClock() {
         let date = new Date();
@@ -38,17 +27,9 @@
         let year = date.getFullYear();
         let day = date.getDay();
 
-        let utcSc = date.getUTCSeconds();
-        let utcMn = date.getUTCMinutes();
-        let utcHr = date.getUTCHours();
-        utcHr = utcHr % 12;
-
         drawArc(sc, mn, hr, clSec, clMin, clHr);
         drawNodes(sc, mn, hr, handSec, handMin, handHr);
         dayAndDate(today, month, year, day, DATE, DAY);
-
-        drawArc(utcSc, utcMn, utcHr, uSec, uMin, uHr);
-        drawNodes(utcSc, utcMn, utcHr, utcHandSec, utcHandMin, utcHandHr);
     }
 
     function dayAndDate(today, month, year, day, longDate, fullDay) {
@@ -116,7 +97,6 @@
         }
         longDate.innerHTML = `${today} ${month} ${year}`;
         fullDay.innerHTML = `${day}`;
-        console.log(`${today} : ${month} : ${year} : ${day}`)
     }
 
     function fillArc(sec, min, hr) {
